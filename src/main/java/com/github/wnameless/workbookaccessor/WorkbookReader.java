d3636e6023ba48646480fca1cad27b8cabb1647e
wmw <wnameless@gmail.com>
2017-11-21 15:52:05 +0800

    Revert last version

--- src/main/java/com/github/wnameless/workbookaccessor/WorkbookReader.java
+++ src/main/java/com/github/wnameless/workbookaccessor/WorkbookReader.java
@@ -458,24 +458,12 @@ public final class WorkbookReader {
       public String apply(Cell item) {
         if (item == null) return "";
 
-        String val;
-        if (item.getCellTypeEnum() == CellType.NUMERIC) {
-          double v = item.getNumericCellValue();
-
-          if ((v == Math.floor(v)) && !Double.isInfinite(v)) {
-            val = Integer.valueOf((int) v).toString();
-          } else {
-            val = Double.toString(v);
-          }
-        } else {
-          val = item.toString();
-        }
-
+        item.setCellType(CellType.STRING);
+        String val = item.toString();
         if (isCSV && val.contains(",")) {
           val = val.replaceAll("\"", "\"\"");
           return '"' + val + '"';
         }
-
         return val;
       }
 
