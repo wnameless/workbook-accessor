fe8dcf3e8df7003ad939b5e2246f6a9540ffb576
wmw <wnameless@gmail.com>
2019-03-20 22:06:28 +0800

    Upgrade pom.xml

--- src/test/java/com/github/wnameless/workbookaccessor/AllWorkbookAccessorTests.java
+++ src/test/java/com/github/wnameless/workbookaccessor/AllWorkbookAccessorTests.java
@@ -23,4 +23,4 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ WorkbookReaderTest.class, WorkbookWriterTest.class })
-public class AllTests {}
+public class AllWorkbookAccessorTests {}
